3e738978358a4d88d9d32aeffcd5c69e628abc5c
Rahul Pathak <rahulzha93@gmail.com>
2019-11-17 17:33:15 +0545

    realPython

---
+++ realPython/basicDatatyps.py
@@ -0,0 +1,62 @@
+#Basic Data Types:
+
+#Integers
+
+print(123123123123123123123123123123123123123123 + 1)
+
+# python 3 ==> no limit to how long an integer can be.
+print(10)
+
+
+# Octal ==> prefix ==> 0o
+print(0o10)
+
+# Octal ==> prefix ==> 0x
+print(0x10)
+
+# Binary ==> prefix ==> 0b
+print(0b10)
+
+# type ==> returns the type
+print(type(10))
+print(type(0o10))
+print(type(0x10))
+# irrespective of the base returns type of int.
+
+
+# Floating-Point Numbers
+print(4.2)
+print(type(4.2))
+print(4.)
+print(.2)
+
+# e or E ==> Scientific Notation
+print(.4e7)
+print(type(.4e7))
+print(4.2e-4)
+
+# max value of float ==> 1.8 x 10^308
+# number greater than max is represented by string inf:
+print(1.79e308)
+print(1.8e308)
+
+# closest non-zero to Zero ==> 5.0 x 10^-324
+print(5e-324)
+print(1e-325)
+
+#Complex Numbers
+print(2+3j)
+print(type(2+3j))
+
+#Strings
+# ==> sequences of character data
+# ==> delimated using either single or double quotes.
+print("I am a string")
+print(type("I am a string"))
+print('I am too.')
+print(type('I am too.'))
+
+# ==> can be as many character as user wish
+# ==> can also be empty
+print('')
+
